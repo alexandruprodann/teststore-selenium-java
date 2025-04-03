7a34290f954dbcd304a21af16378af8d70b5f336
Alexandru Prodan <alexprodan711@gmail.com>
2025-04-03 11:16:01 +0300

    Group tests into specific packages

---
+++ src/main/java/Tests/Desktop/user/AddToWishlistTest.java
@@ -0,0 +1,56 @@
+package Tests.Desktop.user;
+
+import PageObjects.*;
+import Utilities.BaseClass;
+import Utilities.Platform;
+import Utilities.TestConstants;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import static Utilities.TestConstants.PRINTED_SWEATER;
+
+public class AddToWishlistTest extends BaseClass {
+
+    @Test(groups = {Platform.PC})
+    public void signUp() {
+
+        // Pages
+        HomePage homePage = new HomePage();
+        LoginPage loginPage = new LoginPage();
+        ProductPage productPage = new ProductPage();
+        MyWishlistsPage myWishlistsPage = new MyWishlistsPage();
+
+
+        // Steps
+        Reporter.log("Proceed to Homepage: " + TestConstants.BASE_URL);
+        navigateToUrl(TestConstants.BASE_URL);
+
+        Reporter.log("Click Sign In button");
+        homePage.clickSignIn();
+
+        Reporter.log("Login as (" + TestConstants.TEST_EMAIL + "/" + TestConstants.TEST_PASSWORD + ")");
+        loginPage.loginAs(TestConstants.TEST_EMAIL, TestConstants.TEST_PASSWORD);
+
+        Reporter.log("Proceed to Homepage");
+        navigateToUrl(TestConstants.BASE_URL);
+
+        Reporter.log("Add " + PRINTED_SWEATER + " to wishlist");
+        homePage.clickProductByName(PRINTED_SWEATER);
+        productPage.clickAddToWishlist();
+        productPage.clickMyWishlistBtn();
+        Assert.assertTrue(productPage.isWishlistSuccessAlertVisible(), "Wishlist success alert 'Product added' is not displayed!");
+
+        Reporter.log("Navigate to 'My Wishlists'");
+        navigateToUrl(TestConstants.MY_WISHLISTS_URL);
+
+        Reporter.log("Click on 'My Wishlist' link");
+        myWishlistsPage.clickMyWishlistLink();
+
+        Reporter.log("Check if " + PRINTED_SWEATER + " is in the wishlist");
+        Assert.assertTrue(myWishlistsPage.isElementDisplayed(myWishlistsPage.productTitleWithText(PRINTED_SWEATER)), PRINTED_SWEATER + " is not displayed in the wishlist!");
+
+        Reporter.log("Remove the " + PRINTED_SWEATER + " from wishlist");
+        myWishlistsPage.removeProductFromWishlist(PRINTED_SWEATER);
+    }
+}
---
+++ src/main/java/Tests/Mobile/search/SearchBarTest.java
@@ -0,0 +1,39 @@
+package Tests.Mobile.search;
+
+import PageObjects.HomePage;
+import PageObjects.SearchPage;
+import Utilities.BaseClass;
+import Utilities.Platform;
+import Utilities.TestConstants;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SearchBarTest extends BaseClass {
+
+    @Test(groups = {Platform.MOBILE})
+    public void searchBar() {
+
+        String SEARCH_TERM = "Mug";
+
+        // Pages
+        HomePage homePage = new HomePage();
+        SearchPage searchPage = new SearchPage();
+
+
+        // Steps
+        Reporter.log("Access TestStore Homepage: " + TestConstants.BASE_URL);
+        getDriver().get(TestConstants.BASE_URL);
+
+        Reporter.log("Search for: " + SEARCH_TERM);
+        homePage.searchFor(SEARCH_TERM);
+
+        Reporter.log("Verify that search results are displayed");
+        Assert.assertTrue(searchPage.searchResultsDisplayedBool(), "Search results are not displayed!");
+
+        Reporter.log("Verify that search results contain the search term: " + SEARCH_TERM);
+        Assert.assertTrue(searchPage.getSearchResultsText().contains(SEARCH_TERM), "The search result text does not contain the search term: " + SEARCH_TERM);
+
+    }
+
+}
